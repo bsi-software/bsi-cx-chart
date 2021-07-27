335d07ae24108de36a02b3be24ebad76ab84de68
Oliver Fabel <Oliver.Fabel@bsi-software.com>
2021-07-27 14:51:42 +0200

    fix library export with webpack

--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ import ChartConfig from './config';
 import ChartUrlProvider from './chart-url-provider';
 import ChartEnvironment from './environment';
 
-window.ChartConfigColor = ChartConfigColor;
-window.ChartConfig = ChartConfig;
-window.ChartUrlProvider = ChartUrlProvider;
-window.ChartEnvironment = ChartEnvironment;
-
 export {
   ChartConfigColor,
   ChartConfig,
--- webpack.config.js
+++ webpack.config.js
@@ -21,67 +21,74 @@ const banner = [
  * @param {boolean} sourceMap
  * @returns {{}}
  */
-const config = (name, target, sourceMap) => {
-  return {
-    name: name,
-    entry: path.resolve(__dirname, 'src', 'index.js'),
-    mode: sourceMap ? 'development' : 'production',
-    target: 'web',
-    module: {
-      rules: [
-        {
-          test: /\.m?js$/,
-          exclude: /(node_modules|bower_components)/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [['@babel/preset-env', {
-                useBuiltIns: 'usage',
-                shippedProposals: true,
-                corejs: {
-                  version: 3,
-                  proposals: false
-                }
-              }]],
-              sourceType: 'unambiguous'
-            }
+const config = (name, target, sourceMap) => ({
+  name: name,
+  entry: path.resolve(__dirname, 'src', 'index.js'),
+  mode: sourceMap ? 'development' : 'production',
+  target: 'web',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+              useBuiltIns: 'usage',
+              shippedProposals: true,
+              corejs: {
+                version: 3,
+                proposals: false
+              }
+            }]],
+            sourceType: 'unambiguous'
           }
         }
-      ]
-    },
-    devtool: sourceMap ? 'eval-source-map' : false,
-    devServer: sourceMap ? {
-      contentBase: path.resolve(__dirname, 'demo'),
-      compress: true,
-      host: 'localhost',
-      port: 9000,
-      writeToDisk: true
-    } : undefined,
-    optimization: {
-      minimize: true,
-      minimizer: [
-        new TerserWebpackPlugin({
-          extractComments: false,
-          terserOptions: sourceMap ? {
-            sourceMap: true
-          } : undefined
-        })
-      ]
-    },
-    output: {
-      filename: 'chart-url-provider.min.js',
-      path: path.resolve(...target)
-    },
-    performance: {
-      hints: false
-    },
-    plugins: [
-      new WebpackBannerPlugin({
-        banner: banner
+      }
+    ]
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'node_modules'), path.resolve(__dirname, 'src')],
+    extensions: ['.json', '.js']
+  },
+  devtool: sourceMap ? 'eval-source-map' : false,
+  devServer: sourceMap ? {
+    contentBase: path.resolve(__dirname, 'demo'),
+    compress: true,
+    host: 'localhost',
+    port: 9000,
+    writeToDisk: true,
+    inline: false
+  } : undefined,
+  optimization: {
+    minimize: true,
+    minimizer: [
+      new TerserWebpackPlugin({
+        extractComments: false,
+        terserOptions: sourceMap ? {
+          sourceMap: true
+        } : undefined
       })
     ]
-  };
-};
+  },
+  output: {
+    filename: 'chart-url-provider.min.js',
+    path: path.resolve(...target),
+    library: {
+      type: 'umd'
+    },
+    clean: true
+  },
+  performance: {
+    hints: false
+  },
+  plugins: [
+    new WebpackBannerPlugin({
+      banner: banner
+    })
+  ]
+});
 
 module.exports = [
   config('development', [__dirname, 'demo', 'dist'], true),
